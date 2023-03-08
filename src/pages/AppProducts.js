52a7c1a6d00c8f006acbd61d4f6e7f3858b5373f
milicapetrovic23 <milicapetroviccha@gmail.com>
2023-03-08 13:06:10 +0100

    task5

--- src/pages/AppProducts.js
+++ src/pages/AppProducts.js
@@ -1,1 +1,24 @@
-export const AppProducts = () => {};
+import { useEffect, useState } from "react";
+import { ProductsList } from "../components/ProductsList";
+import ProductsService from "../services/ProductsService";
+
+export const AppProducts = () => {
+    const [products, setProducts] = useState([]);
+
+    const handleGetProducts = () => {
+        const prod = ProductsService.getAll()
+        console.log(prod)
+        setProducts(prod)
+    }
+
+    useEffect(() => {
+        handleGetProducts();
+    }, []);
+
+    return (
+        <div>
+            <ProductsList products={products}/>
+        </div>
+    )
+
+};
